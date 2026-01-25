a15db7d6fa6d0ac8cd58ca0e845a1a7720fad377
shanmugam1234skr-pixel <shanmugam1234skr@gmail.com>
2026-01-25 12:26:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,28 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
+CLASS_NAMES = [
+    'Apple___Apple_scab',
+    'Apple___Black_rot',
+    'Apple___Cedar_apple_rust',
+    'Apple___healthy',
+    'Corn_(maize)___Cercospora_leaf_spot Gray',
+    'Corn_(maize)___Common_rust',
+    'Corn_(maize)___healthy',
+    'Corn_(maize)___Northern_Leaf_Blight',
+    'Potato___Early_blight',
+    'Potato___healthy',
+    'Potato___Late_blight',
+    'Tomato___Bacterial_spot',
+    'Tomato___Early_blight',
+    'Tomato___healthy',
+    'Tomato___Late_blight',
+    'Tomato___Leaf_Mold',
+    'Tomato___Septoria_leaf_spot',
+    'Tomato___Spider_mites Two-spotted_spider_mite',
+    'Tomato___Target_Spot'
+]
+
 @st.cache_resource
 def load_model():
     return tf.keras.models.load_model("model.keras", compile=False)
@@ -28,6 +50,9 @@ if uploaded_file:
     class_index = np.argmax(preds)
     confidence = np.max(preds) * 100
 
-    st.success(f"Prediction index: {class_index}")
-    st.info(f"Confidence: {confidence:.2f}%")
+    disease_name = CLASS_NAMES[class_index].replace("___", " → ")
+
+    st.success(f"🦠 Disease: {disease_name}")
+    st.info(f"🔍 Confidence: {confidence:.2f}%")
+
 
